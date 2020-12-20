c09173e85513503f65bf8e0e44ea92c733ba5220
HeatherDyson <60929724+HeatherDyson@users.noreply.github.com>
2020-12-19 22:52:33 -0500

    Add files via upload

---
+++ Advent19b.py
@@ -0,0 +1,69 @@
+import math
+import re
+import pandas as pd
+import numpy as np
+import itertools
+import time
+
+def main():
+
+    fileName = "Day19.txt"
+    #fileName = "Day19example.txt"
+    #fileName = "Day19example2.txt"
+    #read in file
+    f = open(fileName,'r')
+    #START = time.perf_counter_ns()
+
+    rules = {}
+    messages = []
+    for line in f:
+        if re.match('.*:.*', line):
+            line = line.strip('\n')
+            s = line.split(':')
+            if s[1].lstrip() == '"a"':
+                s[1] = 'a'
+            elif s[1].lstrip() == '"b"':
+                s[1] = 'b'
+            elif re.match('.*\|.*', s[1]):
+                s[1] = '(' + s[1] + ' )'
+            else:
+                s[1] = s[1] + ' '
+            rules[int(s[0])] = s[1]
+        else:
+            if line != '\n':
+                messages.append(line.strip('\n'))
+    f.close()
+
+    #modified rules to get regex for rule 11 had to look at reddit
+    #kept adding 42/31 pattern until the answer didn't change
+    rules[8] = ' 42 + '
+    rules[11] = '  42 (?: 42 (?: 42 (?: 42 (?: 42 31 )? 31 )? 31 )? 31 )? 31 '
+    #rules[8] = ' 42 | 42 8 '
+    #rules[11] = ' 42 31 | 42 11 31 '
+    
+    ruleregx = rules[0]
+    counter = 0
+    while re.match('.*\d+.*', ruleregx):
+        ruleNums = set(re.findall('\d+', ruleregx))
+        for r in ruleNums:
+            rule = ruleregx.split()
+            for i in range(len(rule)):
+                if rule[i] == str(r):
+                    rule[i] = rules[int(r)]
+            ruleregx = " ".join(rule)
+        
+        counter += 1
+        if counter > 20:
+            break
+
+    ruleregx = '^' + ruleregx.replace(' ', '') + '$'
+    #print(ruleregx)
+    result = 0
+    for m in messages:
+        if re.search(ruleregx, m):
+            result += 1
+    print("Ans = ", result)
+    #END = time.perf_counter_ns()
+    #print(f"Part 1 took {END-START} nanoseconds.")
+
+main()
